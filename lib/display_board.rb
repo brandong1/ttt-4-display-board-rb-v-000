b7b4f2ae26b26bc03f41ee32df661c6e9a7bc315
Brandon Green <brandon@brandonleeproductions.com>
2018-05-04 23:59:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,15 @@
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
+  
+  
+  
+  
 end
 
 
@@ -17,4 +26,5 @@ end
 # This is completely different than what the lesson says the board[] should be:
 # board = [" "," "," "," "," "," "," "," "," "]
 #
-#I'm still not sure how my other board passed as I had to google how to start a new line since \n hasn't been intoduced yet..
+# I'm still not sure how my other board passed as I had to google how to start a new line since \n hasn't been intoduced yet..
+# Spent way too long with this one.

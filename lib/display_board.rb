359fbc6889503a874a4cb86947d4f06a40a82aa3
Brandon Green <brandon@brandonleeproductions.com>
2018-05-04 23:55:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,14 @@ def display_board(board)
   board = ["   |   |   ","\n,-----------"",\n ""   |   |   ","\n,""-----------"",\n ""   |   |   "]
 
 end
+
+
+#Instructions said copy my board from previous lesson
+#Board from previous lesson:
+# def display_board
+#  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+# 
+# end
+#
+# This is completely different than what the lesson says the board[] should be:
+#
